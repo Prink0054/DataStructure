123b612d0368509afc78bd11bdde18f484706668
Prink0054 <turna.prink@gmail.com>
2021-03-29 18:53:27 +0530

    Reverse a String

---
+++ src/String/ReverseString.java
@@ -0,0 +1,25 @@
+package String;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		System.out.println("////");
+		int a[] = { 3, 1, 5, 0 };
+
+		int start = 0;
+		int end = a.length - 1;
+
+		while (start < end) {
+
+			int m = a[start];
+			a[start] = a[end];
+			a[end] = m;
+			start++;
+			end--;
+		}
+		System.out.println("//");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+}
