d17501f6a97c78f8eb1e3f5548d6e52e619d0d88
Prink0054 <turna.prink@gmail.com>
2021-03-30 20:01:58 +0530

    Left Rotate an Array by D places

--- src/Array/LeftRotateAnArrayByOne.java
+++ src/Array/LeftRotateAnArrayByOne.java
@@ -3,20 +3,33 @@ package Array;
 //0(1) auxilary space contstant space
 //
 public class LeftRotateAnArrayByOne {
+	
+	
+	static void leftone(int a[],int d) {
+		
+		int temp = a[0];
+		for(int i =1;i<a.length;i++) {
+			//moving all emements to left by 1
+			a[i-1] = a[i];
+		}
+		//saving integer value in last of array after roattaing
+		a[a.length-1] =  temp;	
+	}
+	
+	static void leftd(int a[],int d) {
+		for (int i = 0; i < d; i++) {		
+			leftone(a, a.length);
+		}
+	}
+	
+	
+	
 public static void main(String[] args) {
-	
-	
+	int d = 2;
 	int a [] =  {1,2,3,4,5};
 	//storing first elemet in integer
-	int temp = a[0];
 	
-	for(int i =1;i<a.length;i++) {
-		//moving all emements to left by 1
-		a[i-1] = a[i];
-	}
-	//saving integer value in last of array after roattaing
-	a[a.length-1] =  temp;
-	
+	leftd(a,d);
 	for (int i = 0; i < a.length; i++) {
 		System.out.println(a[i]);
 	}
