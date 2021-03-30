44b3e0eacf109f6244fd17c192bc4ef7e28551e8
Prink0054 <turna.prink@gmail.com>
2021-03-30 19:02:38 +0530

    Left Rotate an Array by One

--- src/Array/LeftRotateAnArrayByOne.java
+++ src/Array/LeftRotateAnArrayByOne.java
@@ -7,13 +7,14 @@ public static void main(String[] args) {
 	
 	
 	int a [] =  {1,2,3,4,5};
-	
+	//storing first elemet in integer
 	int temp = a[0];
 	
 	for(int i =1;i<a.length;i++) {
-		
+		//moving all emements to left by 1
 		a[i-1] = a[i];
 	}
+	//saving integer value in last of array after roattaing
 	a[a.length-1] =  temp;
 	
 	for (int i = 0; i < a.length; i++) {
